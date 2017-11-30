28df2637d5e2e82ba5719358e5612397e49df639
ogabeezle <32842826+ogabeezle@users.noreply.github.com>
2017-11-30 23:09:50 +0700

    Add files via upload
    cp, touch, ls, cd, mkdir, rm, expand, pwd, clear kelar

--- xv6-public/tesflush.c
+++ xv6-public/tesflush.c
@@ -3,21 +3,23 @@
 
 int main () {
 
-   char buff[1024];
+   //char buff[1024];
    
-   memset( buff, '\0', sizeof( buff ));
+   //memset( buff, '\0', sizeof( buff ));
    
    //fprintf(stdout, "Going to set full buffering on\n");
-   setvbuf(stdout, buff, _IOFBF, 1024);
+   //setvbuf(stdout, buff, _IOFBF, 1024);
 
    //fprintf(stdout, "This is tutorialspoint.com\n");
    //fprintf(stdout, "This output will go into buff\n");
-   fflush( stdout );
+   
+    printf("\033[2J\033[1;1H\n");
+   //fflush( stdout );
 
    //fprintf(stdout, "and this will appear when programm\n");
   // fprintf(stdout, "will come after sleeping 5 seconds\n");
    
-   sleep(5);
+   //sleep(5);
    
    return(0);
 }
